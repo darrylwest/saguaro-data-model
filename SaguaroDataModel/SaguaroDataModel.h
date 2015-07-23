87af562de4ca1c25aed4e57f9b1229d6b11601ad
darryl west <darryl.west@RainCitySoftware.com>
2015-07-23 10:58:28 -0700

    added initial xcode framework project

---
+++ SaguaroDataModel/SaguaroDataModel.h
@@ -0,0 +1,19 @@
+//
+//  SaguaroDataModel.h
+//  SaguaroDataModel
+//
+//  Created by darryl west on 7/23/15.
+//  Copyright © 2015 darryl west. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SaguaroDataModel.
+FOUNDATION_EXPORT double SaguaroDataModelVersionNumber;
+
+//! Project version string for SaguaroDataModel.
+FOUNDATION_EXPORT const unsigned char SaguaroDataModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SaguaroDataModel/PublicHeader.h>
+
+
